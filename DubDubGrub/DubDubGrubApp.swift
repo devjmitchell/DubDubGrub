9b0e2f72cad6d437af7aed978e8e80e0c007df50
Jason Mitchell <devjmitchell@gmail.com>
2021-06-18 06:48:57 -0700

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Jason Mitchell on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

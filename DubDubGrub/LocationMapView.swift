223919f6c69817a9e2b84692b60920843da84cc8
Jason Mitchell <devjmitchell@gmail.com>
2021-06-19 06:56:46 -0700

    Add the map

--- DubDubGrub/LocationMapView.swift
+++ DubDubGrub/LocationMapView.swift
@@ -5,11 +5,31 @@
 //  Created by Jason Mitchell on 6/18/21.
 //
 
+import MapKit
 import SwiftUI
 
 struct LocationMapView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054),
+        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+    )
+    
     var body: some View {
-        Text("Map View")
+        ZStack {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("ddg-map-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 70)
+                    .shadow(radius: 10)
+                
+                Spacer()
+            }
+        }
     }
 }
 

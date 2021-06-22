b837a2623083188e198b6aa0c49c68151f081dfa
Jason Mitchell <devjmitchell@gmail.com>
2021-06-22 08:59:28 -0700

    Add NavigationLink and ScrollView

--- DubDubGrub/LocationDetailView.swift
+++ DubDubGrub/LocationDetailView.swift
@@ -15,80 +15,86 @@ struct LocationDetailView: View {
     ]
     
     var body: some View {
-        NavigationView {
-            VStack(spacing: 16) {
-                Image("default-banner-asset")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 120)
+        VStack(spacing: 16) {
+            Image("default-banner-asset")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 120)
+            
+            HStack {
+                Label("123 Main Street", systemImage: "mappin.and.ellipse")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
                 
-                HStack {
-                    Label("123 Main Street", systemImage: "mappin.and.ellipse")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                    
-                    Spacer()
-                }
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+            Text("This is a test description. This is a test description. This is a test description. This is a test description. This is a test description.")
+                .lineLimit(3)
+                .minimumScaleFactor(0.75)
+                .frame(height: 70)
                 .padding(.horizontal)
+            
+            ZStack {
+                Capsule()
+                    .frame(height: 80)
+                    .foregroundColor(Color(.secondarySystemBackground))
                 
-                Text("This is a test description. This is a test description. This is a test description. This is a test description. This is a test description.")
-                    .lineLimit(3)
-                    .minimumScaleFactor(0.75)
-                    .padding(.horizontal)
-                
-                ZStack {
-                    Capsule()
-                        .frame(height: 80)
-                        .foregroundColor(Color(.secondarySystemBackground))
+                HStack(spacing: 20) {
+                    Button {
+                        
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "location.fill")
+                    }
                     
-                    HStack(spacing: 20) {
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "location.fill")
-                        }
+                    Link(destination: URL(string: "https://www.apple.com")!, label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "network")
+                    })
+                    
+                    Button {
                         
-                        Link(destination: URL(string: "https://www.apple.com")!, label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "network")
-                        })
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
+                    }
+                    
+                    Button {
                         
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
-                        }
-                        
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
-                        }
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
                     }
                 }
-                .padding(.horizontal)
-                
-                Text("Who's Here?")
-                    .bold()
-                    .font(.title2)
-                
+            }
+            .padding(.horizontal)
+            
+            Text("Who's Here?")
+                .bold()
+                .font(.title2)
+            
+            ScrollView {
                 LazyVGrid(columns: columns, content: {
                     FirstNameAvatarView(firstName: "Jason")
                     FirstNameAvatarView(firstName: "Jason")
                     FirstNameAvatarView(firstName: "Jason")
                     FirstNameAvatarView(firstName: "Jason")
+                    FirstNameAvatarView(firstName: "Jason")
+                    FirstNameAvatarView(firstName: "Jason")
+                    FirstNameAvatarView(firstName: "Jason")
                 })
-                
-                Spacer()
             }
-            .navigationTitle("Location Name")
-            .navigationBarTitleDisplayMode(.inline)
+            
+            Spacer()
         }
+        .navigationTitle("Location Name")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct LocationDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationDetailView()
+        NavigationView {
+            LocationDetailView()
+        }
     }
 }
 

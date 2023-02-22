fc635176644435db4eb4710c7418c9a665195b64
Jason Mitchell <devjmitchell@gmail.com>
2023-02-22 15:01:30 -0500

    Set up AlertItem

---
+++ DubDubGrub/Utilities/AlertItem.swift
@@ -0,0 +1,23 @@
+//
+//  AlertItem.swift
+//  DubDubGrub
+//
+//  Created by Jason Mitchell on 2/22/23.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+
+    // MARK: - MapView Errors
+    static let unableToGetLocations = AlertItem(title: Text("Locations Error"),
+                                                message: Text("Unable to retrieve locations at this time.\nPlease try again."),
+                                                dismissButton: .default(Text("Ok")))
+}

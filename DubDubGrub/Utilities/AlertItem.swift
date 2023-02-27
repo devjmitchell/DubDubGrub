843ee66d5555525d4cfed8b1e5630232fcf4f17f
Jason Mitchell <devjmitchell@gmail.com>
2023-02-27 16:19:04 -0500

    Update existing profile

--- DubDubGrub/Utilities/AlertItem.swift
+++ DubDubGrub/Utilities/AlertItem.swift
@@ -53,4 +53,12 @@ struct AlertContext {
     static let unableToGetProfile = AlertItem(title: Text("Unable To Retrieve Profile"),
                                               message: Text("We were unable to retrieve your profile at this time. Please check your internet connection and try again later or contact customer support if this persists."),
                                               dismissButton: .default(Text("Ok")))
+
+    static let updateProfileSuccess = AlertItem(title: Text("Profile Update Success!"),
+                                                message: Text("Your Dub Dub Grub profile was updated successfully."),
+                                                dismissButton: .default(Text("Sweet!")))
+
+    static let updateProfileFailure = AlertItem(title: Text("Profile Update Failed"),
+                                                message: Text("We were unable to update your profile at this time.\nPlease try again later."),
+                                                dismissButton: .default(Text("Ok")))
 }
--- DubDubGrub/Views/Screens/ProfileView/ProfileViewModel.swift
+++ DubDubGrub/Views/Screens/ProfileView/ProfileViewModel.swift
@@ -7,6 +7,8 @@
 
 import CloudKit
 
+enum ProfileContext { case create, update }
+
 final class ProfileViewModel: ObservableObject {
 
     @Published var firstName = ""
@@ -17,6 +19,11 @@ final class ProfileViewModel: ObservableObject {
     @Published var isShowingPhotoPicker = false
     @Published var isLoading = false
     @Published var alertItem: AlertItem?
+
+    private var existingProfileRecord: CKRecord? {
+        didSet { profileContext = .update }
+    }
+    var profileContext: ProfileContext = .create
 
     func isValidProfile() -> Bool {
 
@@ -50,7 +57,10 @@ final class ProfileViewModel: ObservableObject {
                 hideLoadingView()
 
                 switch result {
-                case .success(_):
+                case .success(let records):
+                    for record in records where record.recordType == RecordType.profile {
+                        existingProfileRecord = record
+                    }
                     alertItem = AlertContext.createProfileSuccess
                 case .failure(_):
                     alertItem = AlertContext.createProfileFailure
@@ -77,15 +87,49 @@ final class ProfileViewModel: ObservableObject {
                 hideLoadingView()
                 switch result {
                 case .success(let record):
+                    existingProfileRecord = record
+
                     let profile = DDGProfile(record: record)
                     firstName = profile.firstName
                     lastName = profile.lastName
                     companyName = profile.companyName
                     bio = profile.bio
                     avatar = profile.createAvatarImage()
+
                 case .failure(_):
                     alertItem = AlertContext.unableToGetProfile
                     break
+                }
+            }
+        }
+    }
+
+    func updateProfile() {
+        guard isValidProfile() else {
+            alertItem = AlertContext.invalidProfile
+            return
+        }
+
+        guard let profileRecord = existingProfileRecord else {
+            alertItem = AlertContext.unableToGetProfile
+            return
+        }
+
+        profileRecord[DDGProfile.kFirstName] = firstName
+        profileRecord[DDGProfile.kLastName] = lastName
+        profileRecord[DDGProfile.kCompanyName] = companyName
+        profileRecord[DDGProfile.kBio] = bio
+        profileRecord[DDGProfile.kAvatar] = avatar.convertToCKAsset()
+
+        showLoadingView()
+        CloudKitManager.shared.save(record: profileRecord) { result in
+            DispatchQueue.main.async { [self] in
+                hideLoadingView()
+                switch result {
+                case .success(_):
+                    alertItem = AlertContext.updateProfileSuccess
+                case .failure(_):
+                    alertItem = AlertContext.updateProfileFailure
                 }
             }
         }
